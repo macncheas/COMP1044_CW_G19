ecce5788510e0ff4869c9d1e4f062b64f12d1ff8
efyqm1 <103098245+efyqm1@users.noreply.github.com>
2022-04-20 18:06:45 +0800

    Add files via upload

---
+++ loginPage.php
@@ -0,0 +1,62 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "MariaDB";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    //Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    if (isset($_POST['username'])) {
+
+        $uname = $_POST['username'];
+        $password = $_POST['password'];
+
+        $sql = "SELECT * FROM `user` WHERE /*Password='.$password' AND */Username='.$uname' limit 1";
+
+        $result = $conn->query($sql);
+        echo "Result obtained<br>";
+        if ($result->num_rows > 0){
+            echo "You have successfully logged in";
+            exit();
+        }
+        else{
+            echo "You have entered incorrect username/password";
+            exit();
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" a href="style.css">
+</head>
+<body>
+
+	<div class="container">
+		<div class="top">
+			<h4> MY LIBRARY</h4>
+		</div>
+	
+		<form method="POST" action="#">
+		
+			<div class="form-input">
+				<input type="text" name="username" placeholder="Username"/>	
+			</div>
+			<div class="form-input">
+				<input type="password" name="password" placeholder="Password"/>
+			</div>
+			<input type="submit" type="submit" value="LOGIN" class="loginbtn"/>
+		</form>
+        
+	</div>
+
+</body>
+</html>
