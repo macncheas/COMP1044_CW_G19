46fa9f470a84d955a05d7312602d13472e516993
macncheas <65759012+macncheas@users.noreply.github.com>
2022-04-23 22:49:08 +0800

    Create deletemember.php

---
+++ deletemember.php
@@ -0,0 +1,22 @@
+<?php
+
+
+include("connection.php");
+
+$id = $_GET['del'];
+$sql = "DELETE FROM member WHERE Member_id = '$id'";
+$data = mysqli_query($conn,$sql);
+
+if($data)
+{
+    echo "deleted";
+    header('Location: members.php');
+    exit();
+}
+else
+{
+    echo 'Failed to delete';
+    exit();
+}
+
+?>
