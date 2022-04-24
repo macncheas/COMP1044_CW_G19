9bf29c7139d893aee48179e1c57a4915218b788a
macncheas <65759012+macncheas@users.noreply.github.com>
2022-04-24 22:40:52 +0800

    Create updatingmembers.php

---
+++ updatingmembers.php
@@ -0,0 +1,30 @@
+<?php
+
+include('connection.php');  
+
+$ID=$_POST['ID'];
+$firstName=$_POST['FirstName'];
+$lastName=$_POST['LastName'];
+$gender=$_POST['Gender'];
+$address=$_POST['Address'];
+$contact=$_POST['Contact'];
+$borrowerType=$_POST['BorrowerType'];
+$yearLevel=$_POST['Year_Level'];
+$status=$_POST['Status'];
+
+if(count($_POST)>0) {
+
+    mysqli_query($conn, "UPDATE `member` SET 
+        `Firstname`='$firstName',
+        `Lastname`='$lastName',
+        `Gender`='$gender',
+        `Address`='$address',
+        `Contact`='$contact'  
+        `Borrowertype`='$borrowerType' 
+        `Year_level`='$yearLevel'  
+        `Status`='$status' WHERE 
+        `Member_id`='$memberID'");
+
+}
+
+?>                                            
