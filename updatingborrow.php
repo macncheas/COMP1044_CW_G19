b19a966096c3e9aaa453c68429e82cd7c2fcbd11
efyqm1 <103098245+efyqm1@users.noreply.github.com>
2022-04-24 14:30:12 +0800

    Add files via upload

---
+++ updatingborrow.php
@@ -0,0 +1,22 @@
+<?php
+    include("connection.php");
+
+    $borrowID = $_POST['BorrowID'];
+    $memberID = $_POST['MemberID'];
+    $dateBorrowed = $_POST['DateBorrowed'];
+    $dueDate = $_POST['DueDate'];
+    $bookID = $_POST['BookID'];
+    $borrowStatus = $_POST['BorrowStatus'];
+    $dateReturned = $_POST['DateReturned'];
+
+    if(count($_POST)>0) {
+        mysqli_query($conn, "UPDATE `borrow` SET 
+        `Member_id`='$memberID',
+        `Date_borrow`='$dateBorrowed',
+        `Due_date`='$dueDate',
+        `book_id`='$bookID',
+        `borrow_status`='$borrowStatus',
+        `date_return`='$dateReturned' WHERE 
+        `Borrow_id`='$borrowID'");
+    }
+?>
